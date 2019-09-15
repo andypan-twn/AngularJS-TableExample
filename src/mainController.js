4359740ab69374093b889b507d434f27eb06e0a4
andy.kuen <andy00005555@hotmail.com>
2019-09-16 01:18:39 +0800

    add: CURD action

--- src/mainController.js
+++ src/mainController.js
@@ -1,40 +1,120 @@
 angular.module('tableExample')
-.controller('MainController',function ($scope){
-    var homeCtrl = this;
+    .controller('MainController', function ($scope, $mdDialog) {
+        var mainCtrl = this;
 
-    homeCtrl.example = {
-        'tableList': [
-            {
-                'name': 'andy',
-                'phone': '0912345678',
-                'email': 'andy.pan@test.com',
-            },
-            {
-                'name': 'uuu',
-                'phone': '22222',
-                'email': 'andy.pan@test.com',
-            },
-        ]
-    };
-
-
-    homeCtrl.add = function(name, phone, email){
-        homeCtrl.example.tableList.push({
-            name: 'aaa',
-            phone: '3333',
-            email: 'andy#sss',
-        });
-    };
-
-    homeCtrl.edit = function(index, name, phone, email){
-        homeCtrl.example.tableList[index] = {
-            name: 'bbb',
-            phone: '5555',
-            email: 'xxxx@sdf',
+        mainCtrl.example = {
+            'tableList': [
+                {
+                    'name': 'andy',
+                    'phone': '0912-345-678',
+                    'email': 'andy.pan@test.com',
+                },
+                {
+                    'name': 'user',
+                    'phone': '+886912345678',
+                    'email': 'user@test.com.tw',
+                },
+            ]
         };
-    };
 
-    homeCtrl.del = function(index){
-        homeCtrl.example.tableList.splice(index, 1);
-    };
-});                   
+
+        mainCtrl.add = function (name, phone, email) {
+            mainCtrl.example.tableList.push({
+                name: name,
+                phone: phone,
+                email: email,
+            });
+        };
+
+        mainCtrl.edit = function (idx, name, phone, email) {
+            mainCtrl.example.tableList[idx] = {
+                name: name,
+                phone: phone,
+                email: email,
+            };
+        };
+
+        mainCtrl.del = function (ev, idx) {
+            var userInfo = mainCtrl.example.tableList[idx];
+            var confirm = $mdDialog.confirm()
+                .parent(angular.element(document.querySelector('#popupContainer')))
+                .clickOutsideToClose(true)
+                .title('Are you sure?')
+                .textContent("You will be delete '" + userInfo.name + "' user")
+                .ariaLabel('Delete an user')
+                .targetEvent(ev)
+                .ok('Delete')
+                .cancel('Cancel');
+
+            $mdDialog.show(confirm).then(function () {
+                mainCtrl.example.tableList.splice(idx, 1);
+            }, function () {
+            });
+        };
+
+        mainCtrl.showDialog = function (ev, type, idx) {
+            if (mainCtrl.dialog == undefined && type == 'create') {
+                mainCtrl.dialog = {
+                    type: type,
+                    title: 'Create an user',
+                    submit: 'Create',
+                    name: undefined,
+                    phone: undefined,
+                    email: undefined,
+                };
+            } else if (type == 'edit') {
+                mainCtrl.dialog = {
+                    type: type,
+                    index: idx,
+                    title: 'Edit an user',
+                    submit: 'Update',
+                    name: mainCtrl.example.tableList[idx].name,
+                    phone: mainCtrl.example.tableList[idx].phone,
+                    email: mainCtrl.example.tableList[idx].email,
+                };
+            }
+            $mdDialog.show({
+                contentElement: '#dialog',
+                targetEvent: ev,
+                clickOutsideToClose: true,
+            });
+        };
+
+        mainCtrl.dialogHide = function () {
+            if (mainCtrl.dialog.type == 'edit') {
+                mainCtrl.dialog = undefined;
+            }
+            $mdDialog.hide();
+        }
+
+        mainCtrl.dialogCancel = function () {
+            mainCtrl.dialog = undefined;
+            $mdDialog.cancel();
+        }
+
+        mainCtrl.dialogSubmit = function () {
+            var dialogVal = mainCtrl.dialog;
+
+            if (dialogVal.name == '' || dialogVal.phone == '' || dialogVal.email == '' || dialogVal.name == undefined || dialogVal.phone == undefined || dialogVal.email == undefined) {
+                return;
+            }
+            switch (dialogVal.type) {
+                case 'create':
+                    mainCtrl.add(
+                        dialogVal.name,
+                        dialogVal.phone,
+                        dialogVal.email
+                    );
+                    break;
+                case 'edit':
+                    mainCtrl.edit(
+                        dialogVal.index,
+                        dialogVal.name,
+                        dialogVal.phone,
+                        dialogVal.email
+                    );
+                    break;
+            }
+            $mdDialog.hide();
+        }
+    });                   
--- src/module.js
+++ src/module.js
@@ -1,4 +1,8 @@
 angular.module('tableExample', [
     'ngMaterial', 
     'ngMessages',
-]);
+]).config(function($mdIconProvider) {
+    $mdIconProvider
+        .defaultFontSet('FontAwesome')
+        .fontSet('fa', 'FontAwesome');
+});
