a76c4592be13bc2e6af3f6584c7a30f4af52dcc8
andy.kuen <andy00005555@hotmail.com>
2019-09-15 22:00:11 +0800

    add: dynamic value

--- src/mainController.js
+++ src/mainController.js
@@ -1,4 +1,40 @@
 angular.module('tableExample')
 .controller('MainController',function ($scope){
-    $scope.text = 'hello world';
+    var homeCtrl = this;
+
+    homeCtrl.example = {
+        'tableList': [
+            {
+                'name': 'andy',
+                'phone': '0912345678',
+                'email': 'andy.pan@test.com',
+            },
+            {
+                'name': 'uuu',
+                'phone': '22222',
+                'email': 'andy.pan@test.com',
+            },
+        ]
+    };
+
+
+    homeCtrl.add = function(name, phone, email){
+        homeCtrl.example.tableList.push({
+            name: 'aaa',
+            phone: '3333',
+            email: 'andy#sss',
+        });
+    };
+
+    homeCtrl.edit = function(index, name, phone, email){
+        homeCtrl.example.tableList[index] = {
+            name: 'bbb',
+            phone: '5555',
+            email: 'xxxx@sdf',
+        };
+    };
+
+    homeCtrl.del = function(index){
+        homeCtrl.example.tableList.splice(index, 1);
+    };
 });                   
